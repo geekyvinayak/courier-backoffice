8a892c22ac0f857cf1a9b528eaf05a797ffeb353
vinayak.pandey <vinayak.pandey@krishtechnolabs.com>
2025-02-17 11:04:21 +0530

    wordings change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -122,7 +122,7 @@ const Navbar = () => {
                 className="w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 flex items-center gap-2"
               >
                 <LogoutOutlinedIcon sx={{ fontSize: 20,borderRadius:"0" }} />
-                Log off
+                Log Out
               </button>
             </div>
           )}
